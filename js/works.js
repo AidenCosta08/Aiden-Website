4dea2f2ddde47b34be1a29a1a61bbd7d4955891e
Aiden Costa <Aidencosta2@gmail.com>
2024-07-31 19:36:13 +0530

    Redesign

---
+++ js/works.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    darkModeToggle.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+        const darkCssLink = document.getElementById('dark-css');
+        darkCssLink.disabled = !darkCssLink.disabled;
+    });
+});
